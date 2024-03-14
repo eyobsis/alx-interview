1e35dedf337a37ded745f3df57c617b16e48d4bf
eyobsis <ehtube2020@gmail.com>
2024-03-14 12:57:32 -0700

    pyrhon intervew practice lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,28 +4,28 @@ Module for solving the lockboxes problem.
 """
 
 
-def can_open_all_boxes(locks):
+def canUnlockAll(boxes):
     """
     Determines if all the boxes can be opened.
 
     Args:
-        locks (list of lists): A list of lists representing the locked boxes.
+        boxes (list of lists): A list of lists representing the locked boxes.
             Each box may contain keys to other boxes.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
-    if not locks:
+    if not boxes:
         return False
 
-    visited = [False] * len(locks)
+    visited = [False] * len(boxes)
     visited[0] = True
     queue = [0]
 
     while queue:
         current_box = queue.pop(0)
-        for key in locks[current_box]:
-            if key < len(locks) and not visited[key]:
+        for key in boxes[current_box]:
+            if key < len(boxes) and not visited[key]:
                 visited[key] = True
                 queue.append(key)
 
@@ -34,12 +34,12 @@ def can_open_all_boxes(locks):
 
 if __name__ == "__main__":
     # Example usage
-    locks1 = [[1], [2], [3], [4], []]
-    print(can_open_all_boxes(locks1))  # Output: True
+    boxes1 = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes1))  # Output: True
 
-    locks2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(can_open_all_boxes(locks2))  # Output: True
+    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes2))  # Output: True
 
-    locks3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(can_open_all_boxes(locks3))  # Output: False
+    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes3))  # Output: False
 
