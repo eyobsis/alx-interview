a2cd07034e862b207c2a25d83ba6fd5cf63bbe52
eyobsis <ehtube2020@gmail.com>
2024-03-21 21:31:31 +0000

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''The minimum operations coding challenge.
+'''
+
+def minOperations(n):
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+
+    Args:
+        n (int): The desired number of H characters.
+
+    Returns:
+        int: The minimum number of operations required.
+    '''
+
+    if n <= 1:
+        return n
+
+    operations = 0
+    current_char_count = 1  # Start with one 'H' character
+
+    while current_char_count < n:
+        if n % current_char_count == 0:
+            # Copy all characters and paste
+            operations += 2
+            current_char_count *= 2
+        else:
+            # Paste from clipboard
+            operations += 1
+            current_char_count += current_char_count
+
+    return operations
+
+if __name__ == "__main__":
+    n = 4
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
