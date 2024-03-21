a27b06719ef65a0c52a4e98c76b81c1435821772
eyobsis <ehtube2020@gmail.com>
2024-03-21 21:36:58 +0000

    commit 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,6 +2,7 @@
 '''The minimum operations coding challenge.
 '''
 
+
 def minOperations(n):
     '''Computes the fewest number of operations needed to result
     in exactly n H characters.
@@ -31,9 +32,10 @@ def minOperations(n):
 
     return operations
 
+
 if __name__ == "__main__":
     n = 4
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    print("Min # of ops to reach {} char: {}".format(n, minOperations(n)))
 
     n = 12
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    print("Min # of ops to reach {} char: {}".format(n, minOperations(n)))
