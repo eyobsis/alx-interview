d9b8569725cd58565fc2b9764e6e2bc2f692b499
eyobsis <ehtube2020@gmail.com>
2024-03-29 00:27:00 +0000

    logparsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,33 +7,33 @@ import sys
 
 if __name__ == '__main__':
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+    file_size_total, line_count = 0, 0
+    http_status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    http_status_stats = {k: 0 for k in http_status_codes}
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
+    def print_statistics(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(file_size))
+        for code, count in sorted(stats.items()):
+            if count:
+                print("{}: {}".format(code, count))
 
     try:
         for line in sys.stdin:
-            count += 1
+            line_count += 1
             data = line.split()
             try:
                 status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
+                if status_code in http_status_stats:
+                    http_status_stats[status_code] += 1
+            except IndexError:
                 pass
             try:
-                filesize += int(data[-1])
-            except BaseException:
+                file_size_total += int(data[-1])
+            except (IndexError, ValueError):
                 pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
+            if line_count % 10 == 0:
+                print_statistics(http_status_stats, file_size_total)
+        print_statistics(http_status_stats, file_size_total)
     except KeyboardInterrupt:
-        print_stats(stats, filesize)
+        print_statistics(http_status_stats, file_size_total)
         raise
